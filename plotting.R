7058c4ee1670e82e6beb3b589301d7cc523a6df7
gsan211 <36670832+gsan211@users.noreply.github.com>
2020-08-22 18:44:07 -0400

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -3,10 +3,10 @@ libary(data.table)
 
 
 #figure 1 plotting mutant base frequency in illumina vs sanger sequencing data
+
 df = fread(../illumina_sanger.txt, header =T)
-p <- ggplot(df, aes(x=ref_sanger, y=sanger_Freq	))+ geom_boxplot(fill='#A4A4A4', color="black", outlier.shape = NA)+
-    geom_jitter(shape=16, position=position_jitter(0.2))+ theme_classic()+
-   scale_y_continuous(trans='log10')+ylab("Mutation rate per bp. per gen. (Log10 scale)")+
-  theme(text = element_text(size=15))
-  
-p
+ggplot(df, aes(x = sanger_Freq, y = illumina_freq ))+
+  geom_point(size = 4) +theme_bw(base_size = 15)+
+  theme(panel.grid.minor.x=element_blank(),panel.grid.minor.y=element_blank(),panel.grid.major.x=element_blank(), panel.grid.major.y=element_blank())+
+  xlab("Mutant frequency Sanger")+ylab("Mutatant frequency Illumina")+
+  geom_smooth(method='lm', colour="black", se = FALSE)
