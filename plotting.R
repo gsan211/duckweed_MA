be148206b912f5b4ef49a9540668d37a1f1715a7
gsan211 <36670832+gsan211@users.noreply.github.com>
2020-08-22 18:51:54 -0400

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -4,9 +4,25 @@ libary(data.table)
 
 #figure 1 plotting mutant base frequency in illumina vs sanger sequencing data
 
-df = fread(../illumina_sanger.txt, header =T)
+df = fread(../fig1_illumina_sanger_data.txt, header =T)
 ggplot(df, aes(x = sanger_Freq, y = illumina_freq ))+
   geom_point(size = 4) +theme_bw(base_size = 15)+
   theme(panel.grid.minor.x=element_blank(),panel.grid.minor.y=element_blank(),panel.grid.major.x=element_blank(), panel.grid.major.y=element_blank())+
   xlab("Mutant frequency Sanger")+ylab("Mutatant frequency Illumina")+
   geom_smooth(method='lm', colour="black", se = FALSE)
+
+
+df = fread(../fig2_ma_data.txt, header=F)
+require(ggplot2)
+ggplot(df, aes(x = V4, y = V1, colour = factor(V5), group=V5)) +
+  geom_point(size = 4,position=position_dodge(width=0.6)) +
+  geom_errorbar(aes(ymax = V2, ymin = V3, width = 0.3),position=position_dodge(width=0.6))+
+  #ggtitle("Power 0.2")+
+  theme_bw()+
+  theme(legend.position = c(0.9, 0.8))+
+  #geom_point(aes(colour = factor(V4)))+
+  scale_color_manual(values=c("black", "darkorange3"))+
+  xlab("Mutation Accumulation lines")+ylab("Mutation rate (per bp. per gen.)")+
+  theme(legend.title = element_blank())+
+  theme(panel.grid.minor.x=element_blank(),panel.grid.minor.y=element_blank(),panel.grid.major.x=element_blank(), panel.grid.major.y=element_blank())
+
